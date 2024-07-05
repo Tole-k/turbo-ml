ecc82f367c8d1eda75084d664eff6bc63919bd8b
Dawid64 <dawid.siera@gmail.com>
2024-07-05 12:46:48 +0200

    initiallizing basing package structure

---
+++ quick_ai/__init__.py
@@ -0,0 +1,7 @@
+"""
+Quick-AI
+===
+Library providing all necessary tools to use automatically adjusted Machine Learning techniques.
+We aim to simplify the process of user input to maximum.
+If unexpirienced with machine learning or other domains of artificial inteligence user will be able to with use of this library solve problems that he should not be able to solve with his knowledge, we will know that we fullfilled our gole.
+"""
