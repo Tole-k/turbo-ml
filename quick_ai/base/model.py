06ebc6a150499ab4565f57ad9b5cb8bba79b89ae
Dawid64 <dawid.siera@gmail.com>
2024-07-26 11:35:20 +0200

    Add validation for model training

--- quick_ai/base/model.py
+++ quick_ai/base/model.py
@@ -1,9 +1,38 @@
 from abc import ABC, ABCMeta, abstractmethod
 import pickle
-from typing import List, Iterable
+from typing import List, Iterable, Any
+from quick_ai.utils.error_tools.exceptions import NotTrainedException
+
+__ALL_MODELS__: List[type] = []
 
 
-class Model(ABC):
+class ModelMetaclass(type):
+
+    def __new__(cls, name, bases, dct):
+        new_class = super().__new__(cls, name, bases, dct)
+        new_class._was_trained = False
+
+        training = getattr(new_class, 'train', None)
+        if training:
+            def new_train(self, data: Iterable, target: Iterable):
+                new_class._was_trained = True
+                return training(self, data, target)
+            setattr(new_class, 'train', new_train)
+
+        prediction = getattr(new_class, 'predict', None)
+        if prediction:
+            def new_predict(self, guess: Any):
+                if not new_class._was_trained:
+                    raise NotTrainedException(
+                        'Model must be trained before predicting')
+                return prediction(self, guess)
+            setattr(new_class, 'predict', new_predict)
+        if training and prediction:
+            __ALL_MODELS__.append(new_class)
+        return new_class
+
+
+class Model(metaclass=ModelMetaclass):
     def __init__(self) -> None:
         super().__init__()
         self.model = None
@@ -13,9 +42,13 @@ class Model(ABC):
         pass
 
     @abstractmethod
-    def predict(self, guess: Iterable) -> List:
+    def predict(self, guess: Any) -> List:
         pass
 
     def save(self, path: str) -> None:
         with open(path, 'wb') as file:
             pickle.dump(self, file)
+
+
+def get_model_list() -> List[Model]:
+    return __ALL_MODELS__
