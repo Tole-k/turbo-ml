d920ed01ca47ba8339934db113d4ce5066088973
Anatol Kaczmarek <anatol.kaczmarek@gmail.com>
2024-07-27 13:59:27 +0200

    add model - process converter

--- quick_ai/base/model.py
+++ quick_ai/base/model.py
@@ -1,9 +1,13 @@
-from abc import ABC, ABCMeta, abstractmethod
+from abc import ABC, abstractmethod
 import pickle
 from typing import List, Iterable
+from .process import Process
 
 
 class Model(ABC):
+    input_formats = {Iterable}
+    output_formats = {List}
+
     def __init__(self) -> None:
         super().__init__()
         self.model = None
@@ -19,3 +23,21 @@ class Model(ABC):
     def save(self, path: str) -> None:
         with open(path, 'wb') as file:
             pickle.dump(self, file)
+
+
+class ModelProcess(Process):
+    def __init__(self, model: Model) -> None:
+        self.model = model
+        super().__init__()
+
+    def pr(self, guess: Iterable) -> List:
+        return self.model.predict(guess)
+
+    def tr(self, data: Iterable, target: Iterable) -> None:
+        self.model.train(data, target)
+
+    def available_input_formats(self) -> set:
+        return self.model.input_formats
+
+    def available_output_formats(self) -> set:
+        return self.model.output_formats
