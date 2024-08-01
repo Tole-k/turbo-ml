1c42484c650f2ef3c6707d508b6eb2446affb518
Anatol Kaczmarek <anatol.kaczmarek@gmail.com>
2024-08-01 12:22:06 +0200

    move less important parameters into **options

--- quick_ai/algorithms/g_boost.py
+++ quick_ai/algorithms/g_boost.py
@@ -1,4 +1,5 @@
 from sklearn import ensemble
+from sklearn.datasets import make_hastie_10_2
 from ..base import Model
 from typing import List, Iterable
 
@@ -8,33 +9,16 @@ class GBoostClassifier(Model):
     output_formats = {List[int], List[str]}
 
     def __init__(self, loss='log_loss', learning_rate=0.1, n_estimators=100,
-                 subsample=1.0, criterion='friedman_mse', min_samples_split=2,
-                 min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_depth=3,
-                 min_impurity_decrease=0.0, init=None, random_state=None,
-                 max_features=None, verbose=0, max_leaf_nodes=None, warm_start=False,
-                 validation_fraction=0.1, n_iter_no_change=None, tol=0.0001,
-                 ccp_alpha=0.0) -> None:
+                 subsample=1.0, min_samples_split=2, max_depth=3, **options) -> None:
         super().__init__()
         self.clf = ensemble.GradientBoostingClassifier(
             loss=loss,
             learning_rate=learning_rate,
             n_estimators=n_estimators,
             subsample=subsample,
-            criterion=criterion,
             min_samples_split=min_samples_split,
-            min_samples_leaf=min_samples_leaf,
-            min_weight_fraction_leaf=min_weight_fraction_leaf,
             max_depth=max_depth,
-            min_impurity_decrease=min_impurity_decrease,
-            init=init,
-            random_state=random_state,
-            max_features=max_features,
-            verbose=verbose,
-            max_leaf_nodes=max_leaf_nodes,
-            warm_start=warm_start,
-            validation_fraction=validation_fraction,
-            n_iter_no_change=n_iter_no_change,
-            ccp_alpha=ccp_alpha
+            **options
         )
 
     def train(self, data: Iterable[int | float], target: Iterable) -> None:
@@ -49,36 +33,17 @@ class GBoostRegressor(Model):
     output_formats = {List[float]}
 
     def __init__(self, loss='squared_error', learning_rate=0.1,
-                 n_estimators=100, subsample=1.0, criterion='friedman_mse',
-                 min_samples_split=2, min_samples_leaf=1,
-                 min_weight_fraction_leaf=0.0, max_depth=3,
-                 min_impurity_decrease=0.0, init=None, random_state=None,
-                 max_features=None, alpha=0.9, verbose=0, max_leaf_nodes=None,
-                 warm_start=False, validation_fraction=0.1,
-                 n_iter_no_change=None, tol=0.0001, ccp_alpha=0.0) -> None:
+                 n_estimators=100, subsample=1.0,
+                 min_samples_split=2, max_depth=3, **options) -> None:
         super().__init__()
         self.reg = ensemble.GradientBoostingRegressor(
             loss=loss,
             learning_rate=learning_rate,
             n_estimators=n_estimators,
             subsample=subsample,
-            criterion=criterion,
             min_samples_split=min_samples_split,
-            min_samples_leaf=min_samples_leaf,
-            min_weight_fraction_leaf=min_weight_fraction_leaf,
             max_depth=max_depth,
-            min_impurity_decrease=min_impurity_decrease,
-            init=init,
-            random_state=random_state,
-            max_features=max_features,
-            alpha=alpha,
-            verbose=verbose,
-            max_leaf_nodes=max_leaf_nodes,
-            warm_start=warm_start,
-            validation_fraction=validation_fraction,
-            n_iter_no_change=n_iter_no_change,
-            tol=tol,
-            ccp_alpha=ccp_alpha
+            **options
         )
 
     def train(self, data: Iterable[int | float], target: Iterable) -> None:
@@ -88,11 +53,10 @@ class GBoostRegressor(Model):
         return self.reg.predict(guess)
 
 
-# X, y = make_hastie_10_2(random_state=0)
-# X_train, X_test = X[:2000], X[2000:]
-# y_train, y_test = y[:2000], y[2000:]
-#
-# clf = GBoostClassifier(n_estimators=100, learning_rate=1.0,
-#                        max_depth=1, random_state=0)
-# clf.train(X_train, y_train)
-# print(clf.predict(X_test))
+X, y = make_hastie_10_2(random_state=0)
+X_train, X_test = X[:2000], X[2000:]
+y_train, y_test = y[:2000], y[2000:]
+clf = GBoostClassifier(n_estimators=100, learning_rate=1.0,
+                       max_depth=1, random_state=0)
+clf.train(X_train, y_train)
+print(clf.predict(X_test))
--- quick_ai/algorithms/xg_boost.py
+++ quick_ai/algorithms/xg_boost.py
@@ -1,215 +1,102 @@
 import xgboost as xgb
 from ..base import Model
-from typing import List, Iterable, Optional, Union, Sequence, Dict, Callable
-import numpy as np
+from typing import List, Iterable, Optional
 from sklearn.model_selection import train_test_split
 
 
 class XGBoostClassifier(Model):
-    input_formats = {Iterable[int | float]}
-    output_formats = {List[int]}
+    input_formats = {Iterable[int | float | bool]}
+    output_formats = {List[int] | List[bool]}
 
     def __init__(self,
                  max_depth: Optional[int] = None,
-                 max_leaves: Optional[int] = None,
-                 max_bin: Optional[int] = None,
-                 grow_policy: Optional[str] = None,
                  learning_rate: Optional[float] = None,
                  n_estimators: Optional[int] = None,
                  booster: Optional[str] = None,
                  tree_method: Optional[str] = None,
-                 n_jobs: Optional[int] = None,
                  gamma: Optional[float] = None,
-                 min_child_weight: Optional[float] = None,
-                 max_delta_step: Optional[float] = None,
                  subsample: Optional[float] = None,
                  sampling_method: Optional[str] = None,
-                 colsample_bytree: Optional[float] = None,
-                 colsample_bylevel: Optional[float] = None,
-                 colsample_bynode: Optional[float] = None,
                  reg_alpha: Optional[float] = None,
                  reg_lambda: Optional[float] = None,
-                 scale_pos_weight: Optional[float] = None,
-                 base_score: Optional[float] = None,
-                 random_state: Optional[
-                     Union[np.random.RandomState, np.random.Generator, int]
-                 ] = None,
-                 missing: float = np.nan,
-                 num_parallel_tree: Optional[int] = None,
-                 monotone_constraints: Optional[Union[Dict[str, int], str]] = None,
-                 interaction_constraints: Optional[Union[str,
-                                                         Sequence[Sequence[str]]]] = None,
-                 importance_type: Optional[str] = None,
-                 device: Optional[str] = None,
-                 validate_parameters: Optional[bool] = None,
-                 enable_categorical: bool = False,
-                 feature_types=None,
-                 max_cat_to_onehot: Optional[int] = None,
-                 max_cat_threshold: Optional[int] = None,
-                 multi_strategy: Optional[str] = None,
-                 eval_metric: Optional[Union[str, List[str], Callable]] = None,
                  early_stopping_rounds: Optional[int] = None,
-                 early_stopping: bool = False) -> None:
+                 **options) -> None:
         super().__init__()
-        early_stop = xgb.callback.EarlyStopping(
-            rounds=early_stopping_rounds, metric_name='logloss', data_name='validation_0', save_best=True
-        ) if early_stopping else None
+        if early_stopping_rounds is not None:
+            self.early_stop = True
         self.clf = xgb.XGBClassifier(
             max_depth=max_depth,
-            max_leaves=max_leaves,
-            max_bin=max_bin,
-            grow_policy=grow_policy,
             learning_rate=learning_rate,
             n_estimators=n_estimators,
             booster=booster,
             tree_method=tree_method,
-            n_jobs=n_jobs,
             gamma=gamma,
-            min_child_weight=min_child_weight,
-            max_delta_step=max_delta_step,
             subsample=subsample,
             sampling_method=sampling_method,
-            colsample_bytree=colsample_bytree,
-            colsample_bynode=colsample_bynode,
-            colsample_bylevel=colsample_bylevel,
             reg_alpha=reg_alpha,
             reg_lambda=reg_lambda,
-            scale_pos_weight=scale_pos_weight,
-            base_score=base_score,
-            random_state=random_state,
-            missing=missing,
-            num_parallel_tree=num_parallel_tree,
-            monotone_constraints=monotone_constraints,
-            interaction_constraints=interaction_constraints,
-            importance_type=importance_type,
-            device=device,
-            validate_parameters=validate_parameters,
-            enable_categorical=enable_categorical,
-            feature_types=feature_types,
-            max_cat_to_onehot=max_cat_to_onehot,
-            max_cat_threshold=max_cat_threshold,
-            multi_strategy=multi_strategy,
-            eval_metric=eval_metric,
             early_stopping_rounds=early_stopping_rounds,
-            callbacks=[early_stop],
+            **options
         )
-        self.early_stopping = early_stopping
 
-    def train(self, data: Iterable[int | float], target: Iterable) -> None:
-        if self.early_stopping:
-            x_train, x_val, y_train, y_val = train_test_split(
-                data, target, test_size=0.20)
-            self.clf.fit(x_train, y_train, eval_set=[(x_val, y_val)])
+    def train(self, data: Iterable[int | float | bool], target: Iterable) -> None:
+        if self.early_stop:
+            x_train, x_test, y_train, y_test = train_test_split(
+                data, target, test_size=0.2)
+            self.clf.fit(x_train, y_train, eval_set=[(x_test, y_test)])
         else:
             self.clf.fit(data, target)
 
-    def predict(self, guess: Iterable[int | float]) -> List[int]:
+    def predict(self, guess: Iterable[int | float | bool]) -> List[int] | List[bool]:
         return self.clf.predict(guess)
 
 
 class XGBoostRegressor(Model):
-    input_formats = {Iterable[int | float]}
+    input_formats = {Iterable[int | float | bool]}
     output_formats = {List[float]}
 
     def __init__(self,
                  max_depth: Optional[int] = None,
-                 max_leaves: Optional[int] = None,
-                 max_bin: Optional[int] = None,
-                 grow_policy: Optional[str] = None,
                  learning_rate: Optional[float] = None,
                  n_estimators: Optional[int] = None,
                  booster: Optional[str] = None,
                  tree_method: Optional[str] = None,
-                 n_jobs: Optional[int] = None,
                  gamma: Optional[float] = None,
-                 min_child_weight: Optional[float] = None,
-                 max_delta_step: Optional[float] = None,
                  subsample: Optional[float] = None,
-                 sampling_method: Optional[str] = None,
-                 colsample_bytree: Optional[float] = None,
-                 colsample_bylevel: Optional[float] = None,
-                 colsample_bynode: Optional[float] = None,
                  reg_alpha: Optional[float] = None,
-                 reg_lambda: Optional[float] = None,
-                 scale_pos_weight: Optional[float] = None,
-                 base_score: Optional[float] = None,
-                 random_state: Optional[
-                     Union[np.random.RandomState, np.random.Generator, int]
-                 ] = None,
-                 missing: float = np.nan,
-                 num_parallel_tree: Optional[int] = None,
-                 monotone_constraints: Optional[Union[Dict[str, int], str]] = None,
-                 interaction_constraints: Optional[Union[str,
-                                                         Sequence[Sequence[str]]]] = None,
-                 importance_type: Optional[str] = None,
-                 device: Optional[str] = None,
-                 validate_parameters: Optional[bool] = None,
-                 enable_categorical: bool = False,
-                 feature_types=None,
-                 max_cat_to_onehot: Optional[int] = None,
-                 max_cat_threshold: Optional[int] = None,
-                 multi_strategy: Optional[str] = None,
-                 eval_metric: Optional[Union[str, List[str], Callable]] = None,
                  early_stopping_rounds: Optional[int] = None,
-                 early_stopping: bool = False) -> None:
+                 **options) -> None:
         super().__init__()
-        early_stop = xgb.callback.EarlyStopping(
-            rounds=early_stopping_rounds, metric_name='logloss', data_name='validation_0', save_best=True
-        ) if early_stopping else None
+        if early_stopping_rounds is not None:
+            self.early_stop = True
         self.clf = xgb.XGBRegressor(
             max_depth=max_depth,
-            max_leaves=max_leaves,
-            max_bin=max_bin,
-            grow_policy=grow_policy,
             learning_rate=learning_rate,
             n_estimators=n_estimators,
             booster=booster,
             tree_method=tree_method,
-            n_jobs=n_jobs,
             gamma=gamma,
-            min_child_weight=min_child_weight,
-            max_delta_step=max_delta_step,
             subsample=subsample,
-            sampling_method=sampling_method,
-            colsample_bytree=colsample_bytree,
-            colsample_bynode=colsample_bynode,
-            colsample_bylevel=colsample_bylevel,
             reg_alpha=reg_alpha,
-            reg_lambda=reg_lambda,
-            scale_pos_weight=scale_pos_weight,
-            base_score=base_score,
-            random_state=random_state,
-            missing=missing,
-            num_parallel_tree=num_parallel_tree,
-            monotone_constraints=monotone_constraints,
-            interaction_constraints=interaction_constraints,
-            importance_type=importance_type,
-            device=device,
-            validate_parameters=validate_parameters,
-            enable_categorical=enable_categorical,
-            feature_types=feature_types,
-            max_cat_to_onehot=max_cat_to_onehot,
-            max_cat_threshold=max_cat_threshold,
-            multi_strategy=multi_strategy,
-            eval_metric=eval_metric,
             early_stopping_rounds=early_stopping_rounds,
-            callbacks=[early_stop],
+            **options
         )
-        self.early_stopping = early_stopping
 
-    def train(self, data: Iterable[int | float], target: Iterable) -> None:
-        if self.early_stopping:
-            x_train, x_val, y_train, y_val = train_test_split(
-                data, target, test_size=0.20)
-            self.clf.fit(x_train, y_train, eval_set=[(x_val, y_val)])
+    def train(self, data: Iterable[int | float | bool], target: Iterable) -> None:
+        if self.early_stop:
+            x_train, x_test, y_train, y_test = train_test_split(
+                data, target, test_size=0.2)
+            self.clf.fit(x_train, y_train, eval_set=[(x_test, y_test)])
         else:
             self.clf.fit(data, target)
 
-    def predict(self, guess: Iterable[int | float]) -> List[float]:
+    def predict(self, guess: Iterable[int | float | bool]) -> List[float]:
         return self.clf.predict(guess)
 
-# X, y = load_breast_cancer(return_X_y=True)
-# clf = XGBoostClassifier(
-#     tree_method="hist", early_stopping=True, early_stopping_rounds=2)
+
+# X = np.random.choice([True, False], size=(100, 10))
+# y = np.random.choice([True, False], size=(100))
+# clf = XGBoostRegressor(
+#     tree_method="hist", early_stopping_rounds=2)
 # clf.train(X, y)
 # print(clf.predict(X))
