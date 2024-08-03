257c38738abda83f5f4fb68181c9f30f6ca29736
Dawid64 <dawid.siera@gmail.com>
2024-08-03 11:56:45 +0200

    Add model forecasting

--- quick_ai/forecast/model_forecast.py
+++ quick_ai/forecast/model_forecast.py
@@ -1,10 +1,35 @@
 from typing import *
 from quick_ai.base import Model
+from quick_ai.base.model import get_model_list
+
+
+def evaluate(model: Type[Model], data: Any, target: Any) -> float:
+    # TODO: Simple evaluation function, to be improved and moved somewhere else
+    train_size = int(len(data) * 9/10)
+    model = model()
+    model.train(data[:train_size], target[:train_size])
+    predictions = model.predict(data[train_size:])
+    mse = sum((predictions[i] - target[i]) **
+              2 for i in range(len(predictions))) / len(predictions)
+    return -mse
 
 
 class Forecast:
     def __init__(self) -> None:
         pass
 
-    def predict() -> Model:
+    def predict(self, data, target) -> Model:
         pass
+
+
+class ExhaustiveSearch(Forecast):
+    def __init__(self) -> None:
+        pass
+
+    def predict(self, data, target) -> Model:
+        best_model: Tuple = (None, -float('inf'))
+        for model_cls in get_model_list():
+            value = evaluate(model_cls, data, target)
+            if value > best_model[1]:
+                best_model = (model_cls, value)
+        return best_model[0]()
