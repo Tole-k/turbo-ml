4d93301bd3fc538657af3034368cff52fe871b28
Dawid64 <dawid.siera@gmail.com>
2024-08-04 17:51:31 +0200

    Add option class

---
+++ quick_ai/utils/options.py
@@ -0,0 +1,5 @@
+class option:
+    print_logs: bool = True  # TODO: change default to False before release
+    text_size: int = 40
+    validation: bool = True  # TODO: change default to False before release
+    # TODO: change default to auto-adjustem based on terminal size before release
