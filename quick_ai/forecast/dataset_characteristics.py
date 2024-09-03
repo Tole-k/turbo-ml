b3577a0ffaf54ae2afc669515154549faa102d71
Dawid64 <dawid.siera@gmail.com>
2024-09-03 15:54:17 +0200

    Decorated dataclasses, hidden datasets import, added typehints

--- quick_ai/forecast/dataset_characteristics.py
+++ quick_ai/forecast/dataset_characteristics.py
@@ -1,51 +1,63 @@
+from typing import Optional
+from dataclasses import dataclass
 import numpy as np
 import pandas as pd
-from datasets import *
 
 
+@dataclass
 class TaskDescription:
-    target_features: int = None
-    task: str = None
+    target_features: int
+    task: str
 
     def __str__(self):
         return f"Task: {self.task}\nTarget Features: {self.target_features}"
 
 
+@dataclass
 class TargetDescription:
-    task: str = None
-    num_classes: int = None
-    biggest_class: str = None
-    biggest_class_freq: int = None
-    smallest_class: str = None
-    smallest_class_freq: int = None
-    target_mean: float = None
-    target_std: float = None
-    target_min: float = None
-    target_25: float = None
-    target_50: float = None
-    target_75: float = None
-    target_max: float = None
-    target_median: float = None
-    target_var: float = None
-    target_skew: float = None
-    target_nans: int = None
+    task: str
+    target_nans: int
+
+
+@dataclass
+class TargetDescriptionClassification(TargetDescription):
+    num_classes: int
+    biggest_class: str
+    biggest_class_freq: int
+    smallest_class: str
+    smallest_class_freq: int
 
     def __str__(self):
-        if self.task == 'regression':
-            return f"Mean: {self.target_mean}\nStd: {self.target_std}\nMin: {self.target_min}\n25%: {self.target_25}\n50%: {self.target_50}\n75%: {self.target_75}\nMax: {self.target_max}\nMedian: {self.target_median}\nVariance: {self.target_var}\nSkew: {self.target_skew}\nNans: {self.target_nans}"
-        else:
-            return f"Number of Classes: {self.num_classes}\nBiggest Class: {self.biggest_class}\nBiggest Class Frequency: {self.biggest_class_freq}\nSmallest Class: {self.smallest_class}\nSmallest Class Frequency: {self.smallest_class_freq}\nNans: {self.target_nans}"
+        return f"Number of Classes: {self.num_classes}\nBiggest Class: {self.biggest_class}\nBiggest Class Frequency: {self.biggest_class_freq}\nSmallest Class: {self.smallest_class}\nSmallest Class Frequency: {self.smallest_class_freq}\nNans: {self.target_nans}"
 
 
+@dataclass
+class TargetDescriptionRegression(TargetDescription):
+    target_min: float
+    target_25: float
+    target_50: float
+    target_75: float
+    target_max: float
+    target_mean: float
+    target_std: float
+    target_median: float
+    target_var: float
+    target_skew: float
+
+    def __str__(self):
+        return f"Mean: {self.target_mean}\nStd: {self.target_std}\nMin: {self.target_min}\n25%: {self.target_25}\n50%: {self.target_50}\n75%: {self.target_75}\nMax: {self.target_max}\nMedian: {self.target_median}\nVariance: {self.target_var}\nSkew: {self.target_skew}\nNans: {self.target_nans}"
+
+
+@dataclass
 class DataSetDescription:
-    num_columns: int = None
-    num_rows: int = None
-    number_of_highly_correlated_features: int = None
-    highest_correlation: float = None
-    number_of_lowly_correlated_features: int = None
-    lowest_correlation: float = None
-    highest_eigenvalue: float = None
-    lowest_eigenvalue: float = None
+    num_columns: int
+    num_rows: int
+    number_of_highly_correlated_features: int
+    highest_correlation: float
+    number_of_lowly_correlated_features: int
+    lowest_correlation: float
+    highest_eigenvalue: float
+    lowest_eigenvalue: float
 
     def __str__(self) -> str:
         return f"Number of Columns: {self.num_columns}\nNumber of Rows: {self.num_rows}\nNumber of Highly Correlated Features: {self.number_of_highly_correlated_features}\nHighest Correlation: {self.highest_correlation}\nNumber of Lowly Correlated Features: {self.number_of_lowly_correlated_features}\nLowest Correlation: {self.lowest_correlation}\nHighest Eigenvalue: {self.highest_eigenvalue}\nLowest Eigenvalue: {self.lowest_eigenvalue}"
@@ -55,10 +67,10 @@ class StatisticalParametersExtractor:
     def __init__(self, data: pd.DataFrame, target: pd.Series):
         self.data = data
         self.target = target
-        self.task_description = TaskDescription()
-        self.target_description = TargetDescription()
-        self.dataset_description = DataSetDescription()
-        self.task = None
+        self.task_description: Optional[TaskDescription] = None
+        self.target_description: Optional[TargetDescription] = None
+        self.dataset_description: Optional[DataSetDescription] = None
+        self.task: Optional[str] = None
 
     def detect_task(self, one_hot_encoded: bool = False):
         num_target_features = 1 if len(
@@ -82,57 +94,60 @@ class StatisticalParametersExtractor:
                         self.task = 'multilabel_classification'
                     else:
                         self.task = 'multiclass_classification'
-        self.task_description.target_features = num_target_features
-        self.task_description.task = self.task
+        self.task_description = TaskDescription(
+            target_features=num_target_features, task=self.task)
         return self.task_description
 
-    def describe_target(self):
-        self.target_description.task = self.task
+    def describe_target(self) -> TargetDescriptionClassification | TargetDescriptionRegression:
+        target_nans = self.target.isna().sum()
         if self.task == 'regression':
             description = self.target.describe()
-            self.target_description.target_mean = description['mean']
-            self.target_description.target_std = description['std']
-            self.target_description.target_min = description['min']
-            self.target_description.target_25 = description['25%']
-            self.target_description.target_50 = description['50%']
-            self.target_description.target_75 = description['75%']
-            self.target_description.target_max = description['max']
-            self.target_description.target_median = self.target.median()
-            self.target_description.target_var = self.target.var()
-            self.target_description.target_skew = self.target.skew()
+            target_mean = description['mean']
+            target_std = description['std']
+            target_min = description['min']
+            target_25 = description['25%']
+            target_50 = description['50%']
+            target_75 = description['75%']
+            target_max = description['max']
+            target_median = self.target.median()
+            target_var = self.target.var()
+            target_skew = self.target.skew()
+            self.target_description = TargetDescriptionRegression(self.task, target_mean=target_mean, target_std=target_std, target_min=target_min,
+                                                                  target_25=target_25, target_50=target_50, target_75=target_75, target_max=target_max, target_median=target_median, target_var=target_var, target_skew=target_skew, target_nans=target_nans)
         else:
             description = self.target.astype('object').describe()
-            self.target_description.num_classes = description['unique']
-            self.target_description.biggest_class = description['top']
-            self.target_description.biggest_class_freq = description['freq']
-            self.target_description.smallest_class = self.target.value_counts().idxmin()
-            self.target_description.smallest_class_freq = self.target.value_counts().min()
-        self.target_description.target_nans = self.target.isna().sum()
+            num_classes = description['unique']
+            biggest_class = description['top']
+            biggest_class_freq = description['freq']
+            smallest_class = self.target.value_counts().idxmin()
+            smallest_class_freq = self.target.value_counts().min()
+            self.target_description = TargetDescriptionClassification(
+                self.task, num_classes=num_classes, biggest_class=biggest_class, biggest_class_freq=biggest_class_freq, smallest_class=smallest_class, smallest_class_freq=smallest_class_freq, target_nans=target_nans)
         return self.target_description
 
-    def describe_dataset(self):
-        self.dataset_description.num_columns = len(self.data.columns)
-        self.dataset_description.num_rows = len(self.data)
+    def describe_dataset(self) -> DataSetDescription:
         continous_cols = self.data.select_dtypes(include=[np.number]).columns
         corr_matrix = self.data[continous_cols].corr()
         removed_diag = corr_matrix.mask(
             np.eye(len(corr_matrix), dtype=bool)).abs()
-        self.dataset_description.number_of_highly_correlated_features = int((
-            removed_diag > 0.8).sum().sum())
-        self.dataset_description.highest_correlation = float(
-            removed_diag.max().max())
-        self.dataset_description.number_of_lowly_correlated_features = int((
-            removed_diag < 0.2).sum().sum())
-        self.dataset_description.lowest_correlation = float(
-            removed_diag.min().min())
-        self.dataset_description.highest_eigenvalue = float(
-            np.linalg.eigvals(corr_matrix).max())
-        self.dataset_description.lowest_eigenvalue = float(
-            np.linalg.eigvals(corr_matrix).min())
+
+        self.dataset_description = DataSetDescription(
+            num_columns=len(self.data.columns),
+            num_rows=len(self.data),
+            number_of_highly_correlated_features=int(
+                (removed_diag > 0.8).sum().sum()),
+            highest_correlation=float(removed_diag.max().max()),
+            number_of_lowly_correlated_features=int(
+                (removed_diag < 0.2).sum().sum()),
+            lowest_correlation=float(removed_diag.min().min()),
+            highest_eigenvalue=float(np.linalg.eigvals(corr_matrix).max()),
+            lowest_eigenvalue=float(np.linalg.eigvals(corr_matrix).min())
+        )
         return self.dataset_description
 
 
 if __name__ == "__main__":
+    from datasets import *
     data, target = get_titanic()
     extractor = StatisticalParametersExtractor(data, target)
 
