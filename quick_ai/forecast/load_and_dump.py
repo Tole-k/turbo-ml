7fc869d764a40bca0437bad93b4d5b2c39e67d13
Anatol Kaczmarek <92335424+Tole-k@users.noreply.github.com>
2024-10-06 17:00:22 +0200

    quick f-x

---
+++ quick_ai/forecast/load_and_dump.py
@@ -0,0 +1,6 @@
+import torch
+from quick_ai.forecast.as_meta_model import Best_Model
+model = Best_Model(15, 36)
+model.load_state_dict(torch.load('model.pth'))
+model = model.to('cpu')
+torch.save(model.state_dict(), 'model_cpu.pth')
