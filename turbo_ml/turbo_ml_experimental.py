3f4b5937c4981a8d9206215cff2c033dc70d4839
Dawid64 <dawid.siera@gmail.com>
2024-10-28 18:07:13 +0100

    Proposed changes in structure, for easier error detection and maintanability

--- turbo_ml/turbo_ml_experimental.py
+++ turbo_ml/turbo_ml_experimental.py
@@ -18,7 +18,9 @@ class TurboML_Experimental:
     def __init__(self, dataset: pd.DataFrame, target: Optional[str] = None, device: Literal['cpu', 'cuda', 'mps'] = 'cpu', threads: int = 1, hpo_trials: int = 10, hpo_enabled: bool = False):
         options.device = device
         options.threads = threads
-        algorithm = DummyModel
+        self._algorithm = DummyModel
+        self.model: Model
+        self.hyperparameters = {}
         self._input_check(dataset, target)
         target_data = dataset[target]
         data = dataset.drop(columns=[target])
@@ -31,15 +33,13 @@ class TurboML_Experimental:
             data, target_data, as_dict=True, old=True)
 
         guesser = MetaModelGuesser()
-        algorithm = guesser.predict(dataset_params)
+        self._algorithm = guesser.predict(dataset_params)
 
         if hpo_enabled:
             tuner = HyperTuner()
-            hyperparameters = tuner.optimize_hyperparameters(
-                algorithm, (data, target_data), dataset_params['task'], dataset_params['num_classes'], dataset_params['target_features'], device, hpo_trials, threads)
-            self.model = algorithm(**hyperparameters)
-        else:
-            self.model = algorithm()
+            self.hyperparameters = tuner.optimize_hyperparameters(
+                self._algorithm, (data, target_data), dataset_params['task'], dataset_params['num_classes'], dataset_params['target_features'], device, hpo_trials, threads)
+        self.model = self._algorithm(**self.hyperparameters)
 
         self.model.train(data, target_data)
 
