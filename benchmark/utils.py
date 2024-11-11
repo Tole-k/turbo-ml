c49658737b325f5a0d0b41aa4ba6004b0c4d595a
Adam Tomys <127124203+adamtms@users.noreply.github.com>
2024-11-11 18:06:49 +0000

    Change file type of output to YAML.

--- benchmark/utils.py
+++ benchmark/utils.py
@@ -4,6 +4,7 @@ import json
 import logging
 from glob import glob
 from enum import Enum, auto
+from typing import List
 from collections import defaultdict
 from datetime import datetime
 
@@ -63,34 +64,67 @@ TASK_MAPPING = {"multiclass_classification": Task.MULTICLASS,
                 "binary_classification": Task.BINARY}
 
 
+class YamlWriter:
+    def __init__(self, library_name):
+        self.path: str = f"benchmark/outputs/{library_name}-{datetime.now()}.yaml"
+        self.file = None
+        self.indent_string = "    "
+        self.indent_size = 0
+
+
+    def __enter__(self):
+        if not os.path.exists("benchmark/outputs"):
+            os.makedirs("benchmark/outputs")
+        self.file = open(self.path, "w")
+        return self
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.file.close()
+
+    def increase_indent(self, name: str):
+        self.file.write(self.indent_string * self.indent_size + f"{name}:\n")
+        self.indent_size += 1
+
+    def decrease_indent(self):
+        self.indent_size -= 1
+
+    def change_indent_name(self, name: str):
+        self.decrease_indent()
+        self.increase_indent(name)
+
+    def add_partial_result(self, dataset_name: str, result: List[str]):
+        prefix = self.indent_string * self.indent_size + "- "
+        self.file.write(prefix + f"{dataset_name}: {result}\n")
+
+
+
 class BaseExperiment(abc.ABC):
     def __init__(self):
         self.name = self.__class__.__name__
 
 
     @abc.abstractmethod
-    def rank_families(self, dataset: pd.DataFrame, dataset_name, task: Task, seed, duration: int):
+    def rank_families(self, dataset: pd.DataFrame, dataset_name, task: Task, seed, duration: int) -> List[ClassificationFamily]:
         pass
 
 
-    def perform_experiments(self, seeds):
-        results = defaultdict(dict)
-        for seed in seeds:
-            for duration in TEST_DURATIONS:
-                result = self._perform_experiment(seed, duration)
-                results[seed][duration] = result
-        self._save_to_json(self.name, results)
+    def perform_experiments(self, seeds, durations=TEST_DURATIONS):
+        with YamlWriter(self.name) as writer:
+            self.writer = writer
+            for seed in seeds:
+                writer.increase_indent(seed)
+                for duration in durations:
+                    writer.increase_indent(duration)
+                    self._perform_experiment(seed, duration)
+                    writer.decrease_indent()
+                writer.decrease_indent()
 
 
     def _perform_experiment(self, seed, duration):
-        experiment_results = dict()
-        parameters = self._get_parameters()
-        family_scores = self.__get_family_scores()
-        for (_, dataset_parameter), (_, dataset_scores) in zip(parameters.iterrows(), family_scores.iterrows()):
+        parameters = self._get_parameters()[:3]
+        for (_, dataset_parameter) in parameters.iterrows():
             dataset_name = dataset_parameter["name"]
             num_classes = dataset_parameter["num_classes"]
-            dataset_scores = {_FAMILY_MAPPING[family]: score for family, score in dict(dataset_scores[1:]).items()}
-            max_score = max(dataset_scores.values())
             dataset = self.__get_dataset(dataset_name)
             task = Task.UNKNOWN
             if num_classes == 2:
@@ -103,21 +137,17 @@ class BaseExperiment(abc.ABC):
                 continue
             ranked_families = self.rank_families(dataset, dataset_name, task, seed, duration)
             if ranked_families is None:
+                self.writer.add_partial_result(dataset_name, [])
                 continue
-            scores_list = []
-            current_max_score = 0
+            ranked_families_names = []
             for family in ranked_families:
-                current_max_score = max(current_max_score, dataset_scores.get(family))
-                scores_list.append(current_max_score / max_score)
-            experiment_results[dataset_name] = scores_list
-        return experiment_results
+                if family.name not in ranked_families_names:
+                    ranked_families_names.append(family.name)
+            self.writer.add_partial_result(dataset_name, ranked_families_names)
                 
 
     def _get_parameters(self):
         return pd.read_csv("data/parameters.csv")
-
-    def __get_family_scores(self):
-        return pd.read_csv("data/family_scores.csv")
     
     def __get_dataset(self, name: str):
         csv_path = f"datasets/AutoIRAD-datasets/*{name}*.csv"
@@ -140,10 +170,3 @@ class BaseExperiment(abc.ABC):
             df = pd.read_table(found_dat[0])
             return df.iloc[:, 1:] # first column is index
                       
-
-    def _save_to_json(self, library_name: str, data: dict) -> None:
-        output_path = f"benchmark/outputs/{library_name}-{datetime.now()}.json"
-        if not os.path.exists("benchmark/outputs"):
-            os.makedirs("benchmark/outputs")
-        with open(output_path, "w") as f:
-            json.dump(data, f)
