e5aa4701c539346180d16ea2ff2149586c03a0eb
Adam Tomys <127124203+adamtms@users.noreply.github.com>
2024-11-11 20:32:44 +0000

    Partially update H2o.

--- benchmark/experiment_h2o.py
+++ benchmark/experiment_h2o.py
@@ -1,27 +1,28 @@
-import logging
+from typing import List
 import h2o
 from h2o.automl import H2OAutoML
-
-from utils_old import BaseExperiment, Task
+import pandas as pd
+from utils import BaseExperiment, Task, ClassificationFamily
 
 # Its not really working. Errors because number of rows is too small
 class H2OExperiment(BaseExperiment):
     def __init__(self):
+        super().__init__()
         h2o.init()
-        self.name = "H2O"
 
-    def find_best_model(self, dataset_path, task, duration, train_ratio=0.8):
-        dataset = h2o.import_file(dataset_path)
-        train, test = dataset.split_frame([train_ratio])
-        x = train.columns
-        y = train.columns[-1]
+    def rank_families(self, dataset: pd.DataFrame, _, task: Task, seed, duration: int) -> List[ClassificationFamily]:
+        if task is not Task.BINARY and task is not Task.MULTICLASS:
+            raise NotImplementedError("Non classification task is not implemented")
+        dataset = h2o.H2OFrame(dataset)
+        x = dataset.columns
+        y = dataset.columns[-1]
         x.remove(y)
         if task is Task.BINARY:
-            train[y] = train[y].asfactor()
-            test[y] = test[y].asfactor()
-        aml = H2OAutoML(max_runtime_secs_per_model=duration, sort_metric="accuracy")
-        aml.train(x=x, y=y, training_frame=train)
+            dataset[y] = dataset[y].asfactor()
+        aml = H2OAutoML(max_runtime_secs=duration, sort_metric="accuracy", seed=seed)
+        aml.train(x=x, y=y, training_frame=dataset)
         lb = aml.leaderboard
+        print(lb)
         for model in lb["model_id"]:
             if model := self.find_model_in_string(model):
                 print(model)
@@ -30,4 +31,4 @@ class H2OExperiment(BaseExperiment):
 
 if __name__ == "__main__":
     experiment = H2OExperiment()
-    experiment.perform_experiment()
+    experiment.perform_experiments([1], [15*60])
