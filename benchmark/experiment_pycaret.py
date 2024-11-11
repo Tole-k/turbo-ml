b5bb1a151371b1c0039971f804f585f00a4dada4
Adam Tomys <127124203+adamtms@users.noreply.github.com>
2024-11-11 20:12:40 +0000

    Update pycaret.

--- benchmark/experiment_pycaret.py
+++ benchmark/experiment_pycaret.py
@@ -1,33 +1,50 @@
 import logging
+from typing import List
 import pandas as pd
 from pycaret.classification import ClassificationExperiment
-from utils_old import BaseExperiment, Task
+from utils import BaseExperiment, Task, ClassificationFamily
 
+
+FAMILIES_MAPPING = {
+    "LogisticRegression": ClassificationFamily.LOGISTIC_AND_MULTINOMINAL_REGRESSION,
+    "KNeighborsClassifier": ClassificationFamily.NEAREST_NEIGHBOR_METHOD,
+    "GaussianNB": ClassificationFamily.BAYESIAN_METHOD,
+    "DecisionTreeClassifier": ClassificationFamily.DECISION_TREE,
+    "SGDClassifier": ClassificationFamily.SVM,
+    "SVC": ClassificationFamily.SVM,
+    "GaussianProcessClassifier": ClassificationFamily.BAYESIAN_METHOD, # not sure
+    "MLPClassifier": ClassificationFamily.NEURAL_NETWORK,
+    "RidgeClassifier": ClassificationFamily.GENERALIZED_LINEAR_MODEL,
+    "RandomForestClassifier": ClassificationFamily.RANDOM_FOREST,
+    "QuadraticDiscriminantAnalysis": ClassificationFamily.DISCRIMINANT_ANALYSIS,
+    "AdaBoostClassifier": ClassificationFamily.BOOSTING,
+    "GradientBoostingClassifier": ClassificationFamily.BOOSTING,
+    "LinearDiscriminantAnalysis": ClassificationFamily.DISCRIMINANT_ANALYSIS,
+    "ExtraTreesClassifier": ClassificationFamily.RANDOM_FOREST,
+    "XGBClassifier": ClassificationFamily.BOOSTING,
+    "LGBMClassifier": ClassificationFamily.BOOSTING,
+    "CatBoostClassifier": ClassificationFamily.BOOSTING,
+    "DummyClassifier": ClassificationFamily.OTHER_METHOD,
+}
+
+# pycaret likes to use up more time than allocated
 class PycaretExperiment(BaseExperiment):
-    def __init__(self):
-        self.name = "Pycaret"
-        
-    def __find_best_model_classification(self, dataset_path, duration, train_ratio=0.8):
-        data = pd.read_csv(dataset_path)
-        s = ClassificationExperiment()
-        s.setup(data, target=data.columns[-1], train_size=train_ratio)
-        best_models = s.compare_models(n_select=10, budget_time=duration/60)
-        best_accuracy_model = s.automl(optimize="Accuracy")
-        if model_name := self.find_model_in_string(type(best_accuracy_model).__name__):
-            return model_name
-        logging.warning(f"Model {type(best_accuracy_model).__name__} not found in the list of models")
-        for model in best_models:
-            if model_name := self.find_model_in_string(type(model).__name__):
-                return model_name
-
-        
-    def find_best_model(self, dataset_path, task, duration, train_ratio=0.8):
-        if task is Task.BINARY or task is Task.MULTICLASS:
-            return self.__find_best_model_classification(dataset_path, duration, train_ratio)
-        else:
+    def rank_families(self, dataset: pd.DataFrame, _, task: Task, seed, duration: int) -> List[ClassificationFamily]:
+        if task is not Task.BINARY and task is not Task.MULTICLASS:
             raise NotImplementedError("Non classification task is not implemented")
+        s = ClassificationExperiment()
+        s.setup(dataset, target=dataset.columns[-1], session_id=seed)
+        best_models = s.compare_models(n_select=100, budget_time=duration/60)
+        best_families = []
+        for model in best_models:
+            model_name = model.__class__.__name__
+            if family := FAMILIES_MAPPING.get(model_name):
+                best_families.append(family)
+            else:
+                logging.warning(f"Couldnt find mapping of {model_name} to a family")
+        return best_families
         
 
 if __name__ == "__main__":
     experiment = PycaretExperiment()
-    experiment.perform_experiment()
+    experiment.perform_experiments([0], [60])
