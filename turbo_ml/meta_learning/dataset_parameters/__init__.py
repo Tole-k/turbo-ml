4d000814f0f720eb8e8ef4da67dfbf6bcf37439e
Dawid64 <dawid.siera@gmail.com>
2024-12-14 00:25:15 +0100

    Updates in __init__s

--- turbo_ml/meta_learning/dataset_parameters/__init__.py
+++ turbo_ml/meta_learning/dataset_parameters/__init__.py
@@ -1,2 +1,3 @@
 from .sota import sota_dataset_parameters, SimpleMetaFeatures, CombinedMetaFeatures, StatisticalMetaFeatures, PCAMetaFeatures
 from .dataset_characteristics import StatisticalParametersExtractor
+from .topological import RipserFeatures, BallMapperFeatures
---
+++ turbo_ml/workflow/__init__.py
@@ -0,0 +1,3 @@
+from turbo_ml.workflow.extract_parameters import generate_training_parameters
+from turbo_ml.workflow.algorithms_evaluations import load_algorithms_evaluations
+from turbo_ml.workflow.train_model import train_meta_model
