d99a6fc8bfae4142b7873d4077f7f29c7cbaf6c1
Dawid64 <dawid.siera@gmail.com>
2024-12-16 19:45:56 +0100

    Updated main pipeline

--- turbo_ml/workflow/__main__.py
+++ turbo_ml/workflow/__main__.py
@@ -1,23 +1,18 @@
 from typing import Tuple
 from prefect import flow
-from prefect.task_runners import DaskTaskRunner
-from turbo_ml.workflow import generate_training_parameters, load_algorithms_evaluations, train_meta_model, save_meta_model
-from turbo_ml.meta_learning.dataset_parameters import SimpleMetaFeatures, BallMapperFeatures
-from turbo_ml.workflow.testing import test_TurboML
+from turbo_ml.workflow import generate_training_parameters, train_meta_model, save_meta_model, evaluate_datasets
+from turbo_ml.meta_learning.dataset_parameters import BallMapperFeatures
 
 
 @flow(name='Full Meta Model Workflow', log_prints=True)
-def model() -> Tuple[int]:
-    training_parameters = generate_training_parameters.submit(
-        meta_data_extractor=SimpleMetaFeatures())
-    evaluations = load_algorithms_evaluations.submit()
-    
-    model, preprocessor = train_meta_model(
-        training_parameters, evaluations, epochs=100)
+def full_pipeline() -> Tuple[int]:
+    training_parameters = generate_training_parameters(
+        meta_data_extractor=BallMapperFeatures())
+    evaluations = evaluate_datasets()
+    model, preprocessor = train_meta_model(training_parameters, evaluations)
     save_meta_model(model, preprocessor, 'new_model')
-    test_TurboML('new_model/', SimpleMetaFeatures())
     return model, preprocessor
 
 
 if __name__ == '__main__':
-    model()
+    full_pipeline()
