11448a9155a4fb023d05b16d53ea5b39f5823da1
Dawid64 <dawid.siera@gmail.com>
2024-12-21 23:42:09 +0100

    Refactor dataset parameters and remove unused test file

--- turbo_ml/meta_learning/dataset_parameters/sota.py
+++ turbo_ml/meta_learning/dataset_parameters/sota.py
@@ -1,24 +1,14 @@
 from typing import Literal
 import numpy as np
 import pandas as pd
-from .dataset_characteristics import StatisticalParametersExtractor
-import warnings
-from .base import CombinedMetaFeatures, SimpleMetaFeatures, StatisticalMetaFeatures, PCAMetaFeatures
-from .topological import BallMapperFeatures, RipserFeatures
+from .base import CombinedMetaFeatures
+from .statistical import SimpleMetaFeatures, StatisticalMetaFeatures, PCAMetaFeatures
+from .topological import BallMapperFeatures
 
 
-def sota_dataset_parameters(dataset: pd.DataFrame, target_data: pd.Series, as_dict: bool = False, parameter_type: Literal['statistical', 'old', 'topological'] = 'statistical') -> np.ndarray | dict:
-    if parameter_type == 'old':
-        warnings.warn(
-            "The 'old' parameter is deprecated and will be removed in a future version.", DeprecationWarning)
-        extractor = StatisticalParametersExtractor(dataset, target=target_data)
-        description = extractor.describe_dataset()
-        dictionary = description.dict()
-        if as_dict:
-            return dictionary
-        variables = list(dictionary.values())[2:]
-        # skipping task describing variables (strings)
-        return np.array(variables)
+def sota_dataset_parameters(dataset: pd.DataFrame, target_data: pd.Series, as_dict: bool = False, parameter_type: Literal['statistical', 'topological'] = 'statistical') -> np.ndarray | dict:
     if parameter_type == 'topological':
-        return RipserFeatures()(dataset, target_data, as_dict=as_dict)
-    return CombinedMetaFeatures([SimpleMetaFeatures(), StatisticalMetaFeatures(), PCAMetaFeatures()])(dataset, target_data, as_dict=as_dict)
+        return BallMapperFeatures()(dataset, target_data, as_dict=as_dict)
+    elif parameter_type == 'statistical':
+        return CombinedMetaFeatures([SimpleMetaFeatures(), StatisticalMetaFeatures(), PCAMetaFeatures()])(dataset, target_data, as_dict=as_dict)
+    raise ValueError(f'Parameter type {parameter_type} not found.')
