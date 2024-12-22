b421c17eae85909b06bd8768c662595712cf361d
Dawid64 <dawid.siera@gmail.com>
2024-12-22 12:36:31 +0100

    Fixed test_optuna by changing from removed model

--- tests/meta_learning/test_optuna.py
+++ tests/meta_learning/test_optuna.py
@@ -1,19 +1,19 @@
 from datasets import get_iris, get_breast_cancer
-from turbo_ml.algorithms import AdaBoostClassifier, XGBoostClassifier
+from turbo_ml.algorithms import XGBoostClassifier, SCIKIT_MODELS
 from turbo_ml.meta_learning import HyperTuner
 
 
 def test_HyperTuner():
     tuner = HyperTuner()
     dataset = get_iris()
-    model = AdaBoostClassifier
+    model = SCIKIT_MODELS['GradientBoostingClassifier']
     task = 'classification'
     parameters = tuner.optimize_hyperparameters(
         model, dataset, task, no_classes=3)
 
     assert isinstance(parameters, dict)
     assert len(parameters) > 0
-    assert 'algorithm' in parameters
+    assert 'ccp_alpha' in parameters
     assert 'learning_rate' in parameters
 
 # def test_HyperTuner_2():
