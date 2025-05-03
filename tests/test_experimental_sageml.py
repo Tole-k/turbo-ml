5a76020c0cb93cc438d1f7642179ba589f2f9ca6
Dawid64 <dawid.siera@gmail.com>
2025-05-03 13:40:06 +0200

    Updated requirements

--- tests/test_experimental_sageml.py
+++ tests/test_experimental_sageml.py
@@ -1,4 +1,4 @@
-from sageml import SageML
+from sageml.sageml_experimental import SageML_Experimental
 from sageml.utils import options
 from datasets import get_iris
 
@@ -10,13 +10,13 @@ def test_happy_path():
     dataset.drop(random.index, inplace=True)
     test = random['target']
     random.drop('target', axis=1, inplace=True)
-    turbo_ml = SageML(dataset=dataset, target='target',
-                      device=options.device, threads=options.threads, hpo_trials=10)
-    result = turbo_ml(random)
+    sml = SageML_Experimental(dataset=dataset, target='target',
+                              device=options.device, threads=options.threads)
+    result = sml(random)
     assert result is not None
     assert len(result) == len(test)
     assert all(i in target for i in result)
-    assert turbo_ml.model.__class__.__name__ != 'RandomGuesser'
+    assert sml.model.__class__.__name__ != 'RandomGuesser'
 
 
 if __name__ == '__main__':
