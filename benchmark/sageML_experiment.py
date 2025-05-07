231d4e7708a965db4889dfa993b31766ac04f4c2
Anatol Kaczmarek <92335424+Tole-k@users.noreply.github.com>
2025-05-08 01:22:16 +0200

    some more tweaks

--- benchmark/sageML_experiment.py
+++ benchmark/sageML_experiment.py
@@ -59,7 +59,7 @@ class SageMLExperiment(BaseExperiment):
 
     def rank_families(self, dataset, dataset_name, *_):
         training_frame = self.data[self.data["name"] != dataset_name].copy()
-        model, preprocessor_dataset,_ = train_meta_model(score_dataframe=training_frame, param_dataframe=self.parameters)
+        model, preprocessor_dataset, _ = train_meta_model(score_dataframe=training_frame, param_dataframe=self.parameters, epochs=10000, batch_size=256)
         # required on macbook
         # options.device = "mps"
         options.threads = 1
